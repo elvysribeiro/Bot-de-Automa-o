fabd3f606be7ced961d40720b408afb57d86a3cf
elvysribeiro <elvyssribeiro@gmail.com>
2023-09-27 19:39:58 -0300

    login

--- codigo.py
+++ codigo.py
@@ -1,18 +1,29 @@
 # Passo a Passo do projeto
 # Passo 1: Entrar no sistema da empresa
      #"https://dlp.hashtagtreinamentos.com/python/intensivao/login"
-# Passo 2: Fazer login
-# Passo 3: Importar a base de dados de produtos
-# Passo 4: Cadastrar 1 produto
-# Passo 5: Repetir o cadastro para todos os produtos
+
 import pyautogui
+import time
 
-
-pyautogui.PAUSE = 0.3
-
-
+#  Abrir o chrome
+pyautogui.PAUSE = 1
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
+
+#  entrar no link
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
-pyautogui.write("link")
+pyautogui.write(link)
+pyautogui.press("enter")
+
+#  Esperar o site carregar
+time.sleep(3)
+
+#  Fazer o login
+pyautogui.click(x=375, y=611)
+pyautogui.write("elvyssribeiro@gmail.com")
+
+
+# Passo 3: Importar a base de dados de produtos
+# Passo 4: Cadastrar 1 produto
+# Passo 5: Repetir o cadastro para todos os produtos
