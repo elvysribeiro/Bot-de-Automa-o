163f3065579711d7c65f6fec796d88be65f594ea
elvysribeiro <elvyssribeiro@gmail.com>
2023-09-27 22:07:01 -0300

    Importação de dados concluída

--- codigo.py
+++ codigo.py
@@ -1,30 +1,9 @@
-# Passo a Passo do projeto
-# Passo 1: Entrar no sistema da empresa
-     #"https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 
-import pyautogui
-import time
 
-#  Abrir o chrome
-pyautogui.PAUSE = .5
-pyautogui.press("win")
-pyautogui.write("chrome")
-pyautogui.press("enter")
-
-#  entrar no link
-link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
-pyautogui.write(link)
-pyautogui.press("enter")
-
-#  Esperar o site carregar
-time.sleep(3)
-
-#  Fazer o login
-pyautogui.click(x=-964, y=405)
-pyautogui.write("elvyssribeiro@gmail.com")
-pyautogui.press("tab")
-pyautogui.write("elvys_9035")
-pyautogui.press("enter")
 # Passo 3: Importar a base de dados de produtos
+import pandas
+
+tabela = pandas.read_csv("produtos.csv")
+print(tabela)
 # Passo 4: Cadastrar 1 produto
 # Passo 5: Repetir o cadastro para todos os produtos
